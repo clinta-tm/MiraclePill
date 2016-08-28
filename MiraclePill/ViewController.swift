a310845b87b0e0d4231ff13d89865f179459a97c
thoughtmakers <clintthoughtmakers@stuarts-macbook-pro.local>
2016-08-28 10:28:41 +0200

    Initial Commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Clint @ ThoughtMakers on 28/08/2016.
+//  Copyright © 2016 Clint @ ThoughtMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
